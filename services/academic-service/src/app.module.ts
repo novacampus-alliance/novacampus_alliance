07cfdf14a1a9835174717bf5183ab7f9915f3fb0
Coulibaly <abdoulkarimcoulibaly525@gmail.com>
2026-06-08 14:06:46 +0200

    feat: detection automatique des conflits de salles (#17)

--- services/academic-service/src/app.module.ts
+++ services/academic-service/src/app.module.ts
@@ -7,6 +7,7 @@ import { CampusModule } from './campus/campus.module';
 import { ProgramsModule } from './programs/programs.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RedisModule } from './redis/redis.module';
+import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { RedisModule } from './redis/redis.module';
     AuthModule,
     CampusModule,
     ProgramsModule,
+    SchedulesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
